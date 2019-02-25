46ef1d14fe01e17c70df1bc6fcbda55840793846
Daniel D'Avella <ddavella@stsci.edu>
2019-02-25 10:50:26 -0500

    First commit of new repository

---
+++ main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+import asdf
+# Hack to suppress AsdfDeprecationWarning
+asdf.exceptions.AsdfDeprecationWarning = DeprecationWarning
+
+
+def parse_metadata(asdffile):
+
+    metadata = asdffile['meta']
+    extracted = {
+        'original filename':    metadata['filename'],
+        'file creation date':   metadata['date'],
+        'pipeline version':     metadata['calibration_software_version'],
+        'instrument':           metadata['instrument']['name'],
+        'detector':             metadata['instrument']['detector'],
+        'model type':           metadata['model_type'],
+        'target name':          metadata['target']['catalog_name'],
+        'observation id':       metadata['observation']['obs_id'],
+    }
+    return extracted
+
+
+def get_metadata(filename):
+    with asdf.open(filename,
+                   ignore_unrecognized_tag=True,
+                   ignore_missing_extensions=True) as af:
+        return parse_metadata(af)
+
+
+def main():
+
+    if len(sys.argv) != 2:
+        script = os.path.basename(sys.argv[0])
+        sys.stderr.write('USAGE: {} <filename>\n'.format(script))
+        return 1
+
+    filename = os.path.abspath(sys.argv[1])
+
+    try:
+        metadata = get_metadata(sys.argv[1])
+    except (IndexError, ValueError):
+        sys.stderr.write('File does not appear to be JWST data product\n')
+        return 1
+    except FileNotFoundError:
+        sys.stderr.write("Given file '{}' can't be found \n".format(sys.argv[1]))
+        return 1
+
+    longest = max([len(x) for x in metadata.keys()])
+    fmt_string = '{:' + str(longest) + '}: {}'
+    for key, value in metadata.items():
+        print(fmt_string.format(key, value))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
